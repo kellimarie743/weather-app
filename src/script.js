3312a60f6bf05130d551f6a952ac50782ac1c2c7
kellimarie743 <108831417+kellimarie743@users.noreply.github.com>
2022-08-31 14:28:27 -0700

    remove weekly forecast HTML , created function forecast , created JS "row" to loop through each day forecast - still fake temp data

--- src/script.js
+++ src/script.js
@@ -36,29 +36,28 @@ function currentDate(date) {
   return `${days[dayInfo]}, ${months[monthInfo]} ${dayNumber} at ${hours}:${minutes}`;
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
-
-  forecast.forEach(function (forecastDay) {
+  let forecastHTML = ` <div class="row">`;
+  let days = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
-    <div class="col-3">
-      <div class="forecast-day">${forecastDay.dt}</div>
+    <div class="col-2">
+      <div class="forecast-day">${day}</div>
       <img
-        src="https://ssl.gstatic.com/onebox/weather/${forecast.weather[0].icon}/sunny.png"
-        alt="sunshine"
-        width="33"
+        src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+        alt="sun with clouds"
+        width="36"
       />
-      <div class="forecast-max-min">
-        <span class="forecast-max">${forecastDay.temp.max}°</span>
-        <span class="forecast-low">${forecastDay.temp.min}°</span>
+      <div class="weekly-forecast-temp">
+        <span class="weekly-forecast-high">18°</span>
+        <span class="weekly-forecast-low">12°</span>
       </div>
-    </div>
+    
+  </div> 
   `;
   });
 
@@ -71,12 +70,6 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemp);
-}
-
-function getForecast(coordinates) {
-  let apiKey = "dd8579c5f641bd775219e128e944f6aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemp(response) {
@@ -146,3 +139,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("Los Angeles");
+displayForecast();
