9317cfec63cb8f930b4e8e5dfb000c6e8d3c0025
kellimarie743 <108831417+kellimarie743@users.noreply.github.com>
2022-08-31 17:04:42 -0700

    converted Fahrenheit back to Celsius for practice and greater world comprehension

--- src/script.js
+++ src/script.js
@@ -84,14 +84,14 @@ function displayForecast(response) {
 
 function search(city) {
   let apiKey = "dd8579c5f641bd775219e128e944f6aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemp);
 }
 
 function getForecast(coordinates) {
   let apiKey = "dd8579c5f641bd775219e128e944f6aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -120,7 +120,7 @@ function hereNow(position) {
   let apiKey = "dd8579c5f641bd775219e128e944f6aa";
   let longitude = position.coords.longitude;
   let latitude = position.coords.latitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
 
